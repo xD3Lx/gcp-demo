072c494f7c6f47eb8b9ac7d909440e00dfa30d6b
Mike Kravchenko <mike.kravchenko.ua@gmail.com>
2020-06-29 22:19:41 +0200

    working test app

---
+++ front-end/src/main/java/org/delrius/TestController.java
@@ -0,0 +1,12 @@
+package org.delrius;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/")
+    String index() {
+        return "Test 2";
+    }
+}
